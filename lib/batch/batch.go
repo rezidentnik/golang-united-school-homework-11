032a13b97e5e65ef726e2db6b1bfec83f45deb3b
admin <admin@admin.net>
2022-06-30 20:25:28 +0300

    resolve hw

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -13,6 +13,35 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
+func userLoader(userIdsPool <-chan int64, loadedUsers chan<- user) {
+	for id := range userIdsPool {
+		loadedUsers <- getOne(id)
+	}
+}
+
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+
+	userIdsPool := make(chan int64, n)
+	loadedUsers := make(chan user, n)
+
+	if n < pool {
+		pool = n
+	}
+
+	var i int64 = 0
+	for i = 0; i < pool; i++ {
+		go userLoader(userIdsPool, loadedUsers)
+	}
+
+	for id := int64(0); id < n; id++ {
+		userIdsPool <- id
+	}
+	close(userIdsPool)
+
+	for i = 0; i < n; i++ {
+		res = append(res, <-loadedUsers)
+	}
+	close(loadedUsers)
+
+	return
 }
